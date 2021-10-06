0a2b07a34999a3d81892051423aa3843d0010c97
nvdiep11062000 <you@example.com>
2021-10-06 08:10:57 +0700

    Create_Table_NVD791

---
+++ NguyenVietDiep791/Models/NVD0791.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NguyenVietDiep791.Models
+{
+    public class NVD0791
+    {
+        [Key]
+        public string NVDId { get; set; }
+        public string NVDName { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string NVDGender { get; set; }
+    }
+}
